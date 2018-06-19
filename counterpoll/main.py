a52ecd04c1623e29792c423e1f68174e5940e781
sihuihan88 <hansihuisjtu@gmail.com>
2018-06-19 13:55:01 -0700

    [counterpoll]: add support to enable/disable counter query (#258)
    Signed-off-by: Sihui Han <sihan@microsoft.com>

--- counterpoll/main.py
+++ counterpoll/main.py
@@ -24,6 +24,24 @@ def interval(poll_interval):
         queue_info['POLL_INTERVAL'] = poll_interval
     configdb.mod_entry("FLEX_COUNTER_TABLE", "QUEUE", queue_info)
 
+@queue.command()
+def enable():
+    """ Enable queue counter query """
+    configdb = swsssdk.ConfigDBConnector()
+    configdb.connect()
+    queue_info = {}
+    queue_info['FLEX_COUNTER_STATUS'] = 'enable'
+    configdb.mod_entry("FLEX_COUNTER_TABLE", "QUEUE", queue_info)
+
+@queue.command()
+def disable():
+    """ Disable queue counter query """
+    configdb = swsssdk.ConfigDBConnector()
+    configdb.connect()
+    queue_info = {}
+    queue_info['FLEX_COUNTER_STATUS'] = 'disable'
+    configdb.mod_entry("FLEX_COUNTER_TABLE", "QUEUE", queue_info)
+
 # Port counter commands
 @cli.group()
 def port():
@@ -38,7 +56,25 @@ def interval(poll_interval):
     port_info = {}
     if poll_interval is not None:
         port_info['POLL_INTERVAL'] = poll_interval
-    configdb.mod_entry("FLEX_COUNTER_TABLE", "test", port_info)
+    configdb.mod_entry("FLEX_COUNTER_TABLE", "PORT", port_info)
+
+@port.command()
+def enable():
+    """ Enable port counter query """
+    configdb = swsssdk.ConfigDBConnector()
+    configdb.connect()
+    port_info = {}
+    port_info['FLEX_COUNTER_STATUS'] = 'enable'
+    configdb.mod_entry("FLEX_COUNTER_TABLE", "PORT", port_info)
+
+@port.command()
+def disable():
+    """ Disable port counter query """
+    configdb = swsssdk.ConfigDBConnector()
+    configdb.connect()
+    port_info = {}
+    port_info['FLEX_COUNTER_STATUS'] = 'disable'
+    configdb.mod_entry("FLEX_COUNTER_TABLE", "PORT", port_info)
 
 @cli.command()
 def show():
@@ -48,12 +84,12 @@ def show():
     queue_info = configdb.get_entry('FLEX_COUNTER_TABLE', 'QUEUE')
     port_info = configdb.get_entry('FLEX_COUNTER_TABLE', 'PORT')
     
-    header = ("Type", "Interval")
+    header = ("Type", "Interval", "Status")
     data = []
     if queue_info:
-        data.append(["QUEUE_STAT", queue_info["POLL_INTERVAL"]])
+        data.append(["QUEUE_STAT", queue_info["POLL_INTERVAL"], queue_info["FLEX_COUNTER_STATUS"]])
     if port_info:
-        data.append(["PORT_STAT", port_info["POLL_INTERVAL"]])
+        data.append(["PORT_STAT", port_info["POLL_INTERVAL"], port_info["FLEX_COUNTER_STATUS"]])
 
     print tabulate(data, headers=header, tablefmt="simple", missingval="")
 
