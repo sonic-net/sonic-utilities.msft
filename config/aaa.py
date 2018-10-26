078b8b8ddee120ffe748cd2d99965d34ac464294
kannankvs <kannan_kvs@dell.com>
2018-10-26 22:43:18 +0530

    tacacs management vrf changes (#346)
    * tacacs management vrf changes
    
    * changed -v --vrf vrfname to -m --use-mgmt-vrf and assume the vrfname as mgmt

--- config/aaa.py
+++ config/aaa.py
@@ -161,7 +161,8 @@ default.add_command(passkey)
 @click.option('-a', '--auth_type', help='Authentication type, default pap', type=click.Choice(["chap", "pap", "mschap", "login"]))
 @click.option('-o', '--port', help='TCP port range is 1 to 65535, default 49', type=click.IntRange(1, 65535), default=49)
 @click.option('-p', '--pri', help="Priority, default 1", type=click.IntRange(1, 64), default=1)
-def add(address, timeout, key, auth_type, port, pri):
+@click.option('-m', '--use-mgmt-vrf', help="Management vrf, default is no vrf", is_flag=True)
+def add(address, timeout, key, auth_type, port, pri, use_mgmt_vrf):
     """Specify a TACACS+ server"""
     if not is_ipaddress(address):
         click.echo('Invalid ip address')
@@ -183,6 +184,8 @@ def add(address, timeout, key, auth_type, port, pri):
             data['timeout'] = str(timeout)
         if key is not None:
             data['passkey'] = key
+        if use_mgmt_vrf :
+            data['vrf'] = "mgmt"
         config_db.set_entry('TACPLUS_SERVER', address, data)
 tacacs.add_command(add)
 
