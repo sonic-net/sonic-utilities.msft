da3934ec0b06d5a7f49785a62f13a798695f6509
Wataru Ishida <5915117+ishidawataru@users.noreply.github.com>
2019-01-10 18:09:19 -0800

    avoid using pkg_resources for faster command execution (#426)
    Signed-off-by: Wataru Ishida <ishida@nel-america.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+# https://github.com/ninjaaron/fast-entry_points
+# workaround for slow 'pkg_resources' import
+#
+# NOTE: this only has effect on console_scripts and no speed-up for commands
+# under scripts/. Consider stop using scripts and use console_scripts instead
+#
+# https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
+try:
+    import fastentrypoints
+except ImportError:
+    from setuptools.command import easy_install
+    import pkg_resources
+    easy_install.main(['fastentrypoints'])
+    pkg_resources.require('fastentrypoints')
+    import fastentrypoints
+
 import glob
 from setuptools import setup
 import unittest
