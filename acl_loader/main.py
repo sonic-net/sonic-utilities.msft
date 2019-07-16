c718cab3b06f8e00936a11b84f398596d12fe00c
JaredLius <39107541+JaredLius@users.noreply.github.com>
2019-07-17 00:18:04 +0800

    [acl_loader]: Fix show mirror_session error (#580)
    Signed-off-by: Jared.Liu <Jared.Liu@nephosinc.com>

--- acl_loader/main.py
+++ acl_loader/main.py
@@ -48,7 +48,8 @@ class AclLoader(object):
     ACL_RULE = "ACL_RULE"
     ACL_TABLE_TYPE_MIRROR = "MIRROR"
     ACL_TABLE_TYPE_CTRLPLANE = "CTRLPLANE"
-    MIRROR_SESSION = "MIRROR_SESSION"
+    CFG_MIRROR_SESSION_TABLE = "MIRROR_SESSION"
+    STATE_MIRROR_SESSION_TABLE = "MIRROR_SESSION_TABLE"
     POLICER = "POLICER"
     SESSION_PREFIX = "everflow"
 
@@ -130,9 +131,9 @@ class AclLoader(object):
         Read MIRROR_SESSION table from configuration database
         :return:
         """
-        self.sessions_db_info = self.configdb.get_table(self.MIRROR_SESSION)
+        self.sessions_db_info = self.configdb.get_table(self.CFG_MIRROR_SESSION_TABLE)
         for key in self.sessions_db_info.keys():
-            state_db_info = self.statedb.get_all(self.statedb.STATE_DB, "{}|{}".format(self.MIRROR_SESSION, key))
+            state_db_info = self.statedb.get_all(self.statedb.STATE_DB, "{}|{}".format(self.STATE_MIRROR_SESSION_TABLE, key))
             if state_db_info:
                 status = state_db_info.get("status", "inactive")
             else:
