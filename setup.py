4024019594bc9174374ed3a3b594d8a43299dd07
Danny Allen <52468448+daall@users.noreply.github.com>
2019-09-03 14:21:13 -0700

    [fast-reboot] Check if ASIC config has changed before warm reboot (#621)
    [fast-reboot] Check if ASIC config has changed before fast reboot
    * Adds script to compare checksums for config files between SONiC images
    * Adds pre-check to fast reboot script to confirm config files match before performing fast reboot
    
    Signed-off-by: Danny Allen <daall@microsoft.com>
    
    * Fix formatting
    
    * Incorporate feedback
    
    * Add check for reboot type and override ability
    
    * Clean up error checking

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     },
     scripts=[
         'scripts/aclshow',
+        'scripts/asic_config_check',
         'scripts/boot_part',
         'scripts/coredump-compress',
         'scripts/db_migrator.py',
