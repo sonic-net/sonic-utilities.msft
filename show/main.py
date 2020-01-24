c139a231299e0d65e806bfa4d38f208f053063bb
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2020-01-25 04:48:58 +0800

    Add command line support for thermal control  (#777)
    Add a "show platform fanstatus" command to allow user fetch FAN status data.
    Add a "show platform temperature" command to allow user fetch thermal status data.

--- show/main.py
+++ show/main.py
@@ -1670,6 +1670,20 @@ def ssdhealth(device, verbose, vendor):
     options += " -e" if vendor else ""
     run_command(cmd + options, display_cmd=verbose)
 
+# 'fan' subcommand ("show platform fan")
+@platform.command()
+def fan():
+    """Show fan status information"""
+    cmd = 'fanshow'
+    run_command(cmd)
+
+# 'temperature' subcommand ("show platform temperature")
+@platform.command()
+def temperature():
+    """Show device temperature information"""
+    cmd = 'tempershow'
+    run_command(cmd)
+
 #
 # 'logging' command ("show logging")
 #
