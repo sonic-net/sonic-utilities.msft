99de1678e25fd8499dadb2044802a1b6ab388b50
madhanmellanox <62459540+madhanmellanox@users.noreply.github.com>
2020-11-04 20:25:15 -0800

    [Mellanox] SKU creator Tool (#1163)
    I added a new script file sonic_sku_create.py to generate a new HWSKU based on XML file or Minigraph. But, focus is on creating HWSKU based on XML file and not on the minigraph and l2 mode. I also added a unit test pyTest script sku_create_test.py in sonic-utilities-test folder to test the script in Unit Testing mode.
    Motivation: To create SKU for Mellanox platforms based on XML file with Port related inputs and also through Minigraph file.
    This tool also allows to split a port or unsplit ports based on configuration which modifies the port related information in port_config.ini and config_db.json
    
    usage: sonic_sku_create.py [-h] [-v]
    (-f FILE | -m [MINIGRAPH_FILE] | -j JSON_FILE | -pp PORT_SPLIT PORT_SPLIT)
    [-b BASE] [-r] [-k HWSKU] [-p] [-vv]
    
    Create a new SKU
    
    optional arguments:
    -h, --help show this help message and exit
    -v, --version show program's version number and exit
    -f FILE, --file FILE SKU definition from xml file. -f OR -m or -j must be provided when creating a new SKU
    -m [MINIGRAPH_FILE], --minigraph_file [MINIGRAPH_FILE]
    SKU definition from minigraph file. -f OR -m or -j must be provided when creating a new SKU
    -j JSON_FILE, --json_file JSON_FILE
    SKU definition from config_db.json file. -f OR -m OR -j must be provided when creating a new SKU
    -pp PORT_SPLIT PORT_SPLIT, --port_split PORT_SPLIT PORT_SPLIT
    port name and split
    -b BASE, --base BASE SKU base definition
    -r, --remove Remove SKU folder
    -k HWSKU, --hwsku HWSKU
    SKU name to be used when creating a new SKU or for L2 configuration mode
    -p, --print Print port_config.ini without creating a new SKU
    -vv, --verbose Verbose output

---
+++ sonic-utilities-tests/sku_create_test.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import pytest
+import subprocess
+import re
+
+test_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(test_path)
+scripts_path = os.path.join(modules_path, "scripts")
+input_path = os.path.join(modules_path, "sonic-utilities-tests/sku_create_input")
+sku_def_file = os.path.join(input_path, "Mellanox-SN2700-D48C8.xml")
+sku_create_script = os.path.join(scripts_path, "sonic_sku_create.py")
+output_file_path = os.path.join(modules_path, "sonic-utilities-tests/sku_create_input/Mellanox-SN2700-D48C8_NEW/port_config.ini")
+model_file_path = os.path.join(modules_path, "sonic-utilities-tests/sku_create_input/Mellanox-SN2700-D48C8/port_config.ini")
+
+sys.path.insert(0, test_path)
+sys.path.insert(0, modules_path)
+
+class TestSkuCreate(object):
+    @classmethod
+    def setup_class(cls):
+        os.environ["PATH"] += os.pathsep + scripts_path
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    def are_file_contents_same(self,fname1,fname2):
+        #Open the file for reading in text mode (default mode)
+        f1 = open(fname1)
+        f2 = open(fname2)
+
+        #Read the first line from the files
+        f1_line = f1.readline()
+        f2_line = f2.readline()
+
+        #Loop if either fname1 or fname2 has not reached EOF
+        while f1_line!='' or f2_line!='':
+            f1_line = re.sub('[\s+]','',f1_line)
+            f2_line = re.sub('[\s+]','',f2_line)
+
+            if f1_line!=f2_line:
+                f1.close()
+                f2.close()
+                return False
+            else:
+                f1_line = f1.readline()
+                f2_line = f2.readline()
+
+        f1.close()
+        f2.close()
+        return True
+    
+    def test_no_param(self):
+        my_command = sku_create_script + " -f "  + sku_def_file  + " -d " + input_path
+
+        #Test case execution without stdout
+        result = subprocess.check_output(my_command,stderr=subprocess.STDOUT,shell=True)
+        print result
+
+        #Check if the Output file exists
+        if (os.path.exists(output_file_path)):
+            print("Output file: ",output_file_path,"exists. SUCCESS!")
+        else:
+            pytest.fail("Output file: {} does not exist. FAILURE!".format(output_file_path))
+
+        #Check if the Output file and the model file have same contents
+        if self.are_file_contents_same(output_file_path,model_file_path) == True:
+            print("Output file: ",output_file_path," and model file: ",model_file_path,"contents are same. SUCCESS!")
+        else:
+            pytest.fail("Output file: {} and model file: {} contents are not same. FAILURE!".format(output_file_path,model_file_path))
+
+    @classmethod
+    def teardown_class(cls):
+        print("TEARDOWN")
+        os.environ["PATH"] = os.pathsep.join(os.environ["PATH"].split(os.pathsep)[:-1])
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
