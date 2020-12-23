ff1db4db76c48ffcad91a901f444595c69e97165
jangidrk <ramakant.jangid@broadcom.com>
2020-12-24 03:34:57 +0530

    [fast-reboot]: Update fast-reboot-dump.py to handle invalid parsing of genetlink type hostif object id attribute (#1318)
    Fix fast reboot failure due to invalid parsing of genetlink type hostif object id attribute. Genetlink type hostif does NOT have object ID attribute.
    SAI_HOSTIF_ATTR_OBJ_ID (SAI_HOSTIF_ATTR_TYPE == SAI_HOSTIF_TYPE_GENETLINK)

--- scripts/fast-reboot-dump.py
+++ scripts/fast-reboot-dump.py
@@ -86,6 +86,8 @@ def get_map_port_id_2_iface_name(db):
     keys = [] if keys is None else keys
     for key in keys:
         value = db.get_all(db.ASIC_DB, key)
+        if value['SAI_HOSTIF_ATTR_TYPE'] != 'SAI_HOSTIF_TYPE_NETDEV':
+            continue
         port_id = value['SAI_HOSTIF_ATTR_OBJ_ID']
         iface_name = value['SAI_HOSTIF_ATTR_NAME']
         port_id_2_iface[port_id] = iface_name
