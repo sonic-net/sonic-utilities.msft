87b24818b47c3f6cb144e01eec19be91623613b6
abdosi <58047199+abdosi@users.noreply.github.com>
2021-04-04 12:17:22 -0700

    Reduce time taken by show commands on multi-asic platforms (#1544)
    Optimize/Reduce the time taken by show interface * and show ip bgp * commands on Multi-asic platforms.

--- utilities_common/multi_asic.py
+++ utilities_common/multi_asic.py
@@ -30,11 +30,11 @@ class MultiAsic(object):
         For single asic, this function is not applicable
         '''
         if object_type == constants.PORT_OBJ:
-            return multi_asic.is_port_internal(cli_object)
+            return multi_asic.is_port_internal(cli_object, self.current_namespace)
         elif object_type == constants.PORT_CHANNEL_OBJ:
-            return multi_asic.is_port_channel_internal(cli_object)
+            return multi_asic.is_port_channel_internal(cli_object, self.current_namespace)
         elif object_type == constants.BGP_NEIGH_OBJ:
-            return multi_asic.is_bgp_session_internal(cli_object)
+            return multi_asic.is_bgp_session_internal(cli_object, self.current_namespace)
 
     def skip_display(self, object_type, cli_object):
         '''
