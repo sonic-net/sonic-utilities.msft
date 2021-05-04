0f4988bc285ee14aa383e004a42d2043716713fa
Andriy Yurkiv <70649192+ayurkiv-nvda@users.noreply.github.com>
2021-05-04 10:35:32 +0300

    Add pg-drop script to sonic filesystem (#1583)
    - What I did
    Added 'pg-drop' to the files system of SONiC
    
    - How I did it
    Add 'scripts/pg-drop' to setup.py file
    
    - How to verify it
    Check that 'pg-drop' script exists in /usr/local/bin/pg-drop
    
    Signed-off-by: Andriy Yurkiv <ayurkiv@nvidia.com>

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ setup(
         'scripts/nbrshow',
         'scripts/neighbor_advertiser',
         'scripts/pcmping',
+        'scripts/pg-drop',
         'scripts/port2alias',
         'scripts/portconfig',
         'scripts/portstat',
