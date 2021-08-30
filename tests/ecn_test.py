3e8626ea6e495e44a303c6ce81aa4f04a3dd2df9
Neetha John <nejo@microsoft.com>
2021-08-30 08:27:29 -0700

    [ecnconfig] Fix exception seen during display and add unit tests (#1784)
    Fixes Azure/sonic-buildimage#8323 and added unit tests for all ecn queue configs. This regression was caused because the current ecn unit tests didn't cover the queue configs.
    
    Signed-off-by: Neetha John <nejo@microsoft.com>
    
    How to verify it
    Ran tests/ecn_test.py and all cases passed

--- tests/ecn_test.py
+++ tests/ecn_test.py
@@ -1,3 +1,4 @@
+import ast
 import json
 import os
 import sys
@@ -6,6 +7,8 @@ from click.testing import CliRunner
 
 import config.main as config
 from .ecn_input.ecn_test_vectors import *
+from .utils import get_result_and_return_code
+from utilities_common.db import Db
 import show.main as show
 
 test_path = os.path.dirname(os.path.abspath(__file__))
@@ -25,8 +28,14 @@ class TestEcnConfig(object):
     def test_ecn_show_config(self):
         self.executor(testData['ecn_show_config'])
 
+    def test_ecn_show_config_verbose(self):
+        self.executor(testData['ecn_show_config_verbose'])
+
     def test_ecn_config_gmin(self):
         self.executor(testData['ecn_cfg_gmin'])
+
+    def test_ecn_config_gmin_verbose(self):
+        self.executor(testData['ecn_cfg_gmin_verbose'])
 
     def test_ecn_config_gmax(self):
         self.executor(testData['ecn_cfg_gmax'])
@@ -45,6 +54,9 @@ class TestEcnConfig(object):
 
     def test_ecn_config_gdrop(self):
         self.executor(testData['ecn_cfg_gdrop'])
+
+    def test_ecn_config_gdrop_verbose(self):
+        self.executor(testData['ecn_cfg_gdrop_verbose'])
 
     def test_ecn_config_ydrop(self):
         self.executor(testData['ecn_cfg_ydrop'])
@@ -70,37 +82,95 @@ class TestEcnConfig(object):
     def test_ecn_config_rdrop_invalid(self):
         self.executor(testData['ecn_cfg_rdrop_invalid'])
 
+    def test_ecn_queue_get(self):
+        self.executor(testData['ecn_q_get'])
+
+    def test_ecn_queue_get_verbose(self):
+        self.executor(testData['ecn_q_get_verbose'])
+
+    def test_ecn_all_queue_get(self):
+        self.executor(testData['ecn_q_all_get'])
+
+    def test_ecn_queue_all_get_verbose(self):
+        self.executor(testData['ecn_q_all_get_verbose'])
+
+    def test_ecn_queue_set_q_off(self):
+        self.executor(testData['ecn_cfg_q_off'])
+
+    def test_ecn_queue_set_q_off_verbose(self):
+        self.executor(testData['ecn_cfg_q_off_verbose'])
+
+    def test_ecn_queue_set_all_off(self):
+        self.executor(testData['ecn_cfg_q_all_off'])
+
+    def test_ecn_queue_set_all_off_verbose(self):
+        self.executor(testData['ecn_cfg_q_all_off_verbose'])
+
+    def test_ecn_queue_set_q_on(self):
+        self.executor(testData['ecn_cfg_q_on'])
+
+    def test_ecn_queue_set_q_on_verbose(self):
+        self.executor(testData['ecn_cfg_q_on_verbose'])
+
+    def test_ecn_queue_set_all_on(self):
+        self.executor(testData['ecn_cfg_q_all_on'])
+
+    def test_ecn_queue_set_all_on_verbose(self):
+        self.executor(testData['ecn_cfg_q_all_on_verbose'])
+
     def executor(self, input):
         runner = CliRunner()
 
+        if 'db_table' in input:
+            db = Db()
+            data_list = list(db.cfgdb.get_table(input['db_table']))
+            input['rc_msg'] = input['rc_msg'].format(",".join(data_list))
+
         if 'show' in input['cmd']:
             exec_cmd = show.cli.commands["ecn"]
+            result = runner.invoke(exec_cmd, input['args'])
+            exit_code = result.exit_code
+            output = result.output
+        elif 'q_cmd' in input['cmd'] :
+            exit_code, output = get_result_and_return_code("ecnconfig {}".format(" ".join(input['args'])))
         else:
             exec_cmd = config.config.commands["ecn"]
+            result = runner.invoke(exec_cmd, input['args'])
+            exit_code = result.exit_code
+            output = result.output
 
-        result = runner.invoke(exec_cmd, input['args'])
-
-        print(result.exit_code)
-        print(result.output)
+        print(exit_code)
+        print(output)
 
         if input['rc'] == 0:
-            assert result.exit_code == 0
+            assert exit_code == 0
         else:
-            assert result.exit_code != 0
+            assert exit_code != 0
 
         if 'cmp_args' in input:
             fd = open('/tmp/ecnconfig', 'r')
-            prof_data = json.load(fd)
-            for args in input['cmp_args']:
-                profile, name, value = args.split(',')
-                assert(prof_data[profile][name] == value)
+            cmp_data = json.load(fd)
+
+            if 'cmp_q_args' in input:
+                if 'other_q' in input:
+                    profile1, value1 = input['cmp_args'][-1].split(',')
+                profile, value = input['cmp_args'][0].split(',')
+                for key in cmp_data:
+                    if ast.literal_eval(key)[-1] in input['cmp_q_args']:
+                        assert(cmp_data[key][profile] == value)
+                    if 'other_q' in input and ast.literal_eval(key)[-1] in input['other_q']:
+                        assert(cmp_data[key][profile1] == value1)
+            else:
+                for args in input['cmp_args']:
+                    profile, name, value = args.split(',')
+                    assert(cmp_data[profile][name] == value)
             fd.close()
 
         if 'rc_msg' in input:
-            assert input['rc_msg'] in result.output
+            assert input['rc_msg'] in output
 
         if 'rc_output' in input:
-            assert result.output == input['rc_output']
+            assert output == input['rc_output']
 
     @classmethod
     def teardown_class(cls):
