efa2ff62eef37b28526edc43119ffdfa5ff12682
Mahesh Maddikayala <10645050+smaheshm@users.noreply.github.com>
2021-11-15 09:20:40 -0800

    [show][platform summary] Add chassis type in the platform summary output(#1922)

--- show/platform.py
+++ show/platform.py
@@ -64,6 +64,9 @@ def summary(json):
         click.echo("Serial Number: {}".format(chassis_info['serial']))
         click.echo("Model Number: {}".format(chassis_info['model']))
         click.echo("Hardware Revision: {}".format(chassis_info['revision']))
+        switch_type = platform_info.get('switch_type')
+        if switch_type:
+            click.echo("Switch Type: {}".format(switch_type))
 
 
 # 'syseeprom' subcommand ("show platform syseeprom")
