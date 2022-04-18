781ae9f57174764d12fea752fcb313c7c701f2c1
Jing Kan <jika@microsoft.com>
2022-04-18 12:54:34 +0800

    [config] Do not enable pfcwd for BmcMgmtToRRouter (#2136)
    Signed-off-by: Jing Kan jika@microsoft.com

--- config/main.py
+++ config/main.py
@@ -1604,7 +1604,7 @@ def load_minigraph(db, no_service_restart):
 
     # get the device type
     device_type = _get_device_type()
-    if device_type != 'MgmtToRRouter' and device_type != 'MgmtTsToR' and device_type != 'EPMS':
+    if device_type != 'MgmtToRRouter' and device_type != 'MgmtTsToR' and device_type != 'BmcMgmtToRRouter' and device_type != 'EPMS':
         clicommon.run_command("pfcwd start_default", display_cmd=True)
 
     # Write latest db version string into db
