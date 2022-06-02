3197f3996ef8173ef4993aa33a1cba60fd4cab52
anilkpan <64167306+anilkpan@users.noreply.github.com>
2022-06-01 17:11:11 -0700

    Add check to not allow deleting PO if its member of vlan. (#2141)
    * Added check to not allow deleting PO if its member of vlan.

--- tests/portchannel_test.py
+++ tests/portchannel_test.py
@@ -179,6 +179,18 @@ class TestPortChannel(object):
         assert result.exit_code != 0
         assert "Error: Port Ethernet60 is already bound to following PBH_TABLES:" in result.output
 
+    def test_delete_portchannel_which_is_member_of_a_vlan(self):
+        runner = CliRunner()
+        db = Db()
+        obj = {'db':db.cfgdb}
+
+        # try to delete the portchannel when its member of a vlan
+        result = runner.invoke(config.config.commands["portchannel"].commands["del"], ["PortChannel1001"], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code != 0
+        assert "PortChannel1001 has vlan Vlan4000 configured, remove vlan membership to proceed" in result.output
+
     @classmethod
     def teardown_class(cls):
         os.environ['UTILITIES_UNIT_TESTING'] = "0"
