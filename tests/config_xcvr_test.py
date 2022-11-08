d5a6da31ef94e350c2cb66c3c981145524d7de6f
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2022-11-08 19:48:39 +0800

    Do not configure physical attributes on port channels in portconfig (#2456)
    - What I did
    portconfig is a utility designed to configure various attributes on a physical port or a port channel.
    It should reject the operation that a user wants to configure an attribute that will be inserted into CONFIG_DB.PORT table on a port channel. Otherwise, it will generate an entry for the port channel, which causes orchagent to be aborted.
    
    Eg. the following commands will cause an error
    
    admin@sonic:~$ sudo config portchannel add PortChannel0001
    admin@sonic:~$ sudo config interface speed PortChannel0001 100000
    
    After the fix, configuring physical attributes on a port channel will be rejected
    
    - How I did it
    
    - How to verify it
    Unit test
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- tests/config_xcvr_test.py
+++ tests/config_xcvr_test.py
@@ -34,12 +34,18 @@ class TestConfigXcvr(object):
         assert "Setting laser frequency" in result.output
         result = self.basic_check("frequency", ["Ethernet0", "--", "-1"], ctx, op=operator.ne)
         assert "Error: Frequency must be > 0" in result.output
+        # Setting laser frequency on a port channel is not supported
+        result = self.basic_check("frequency", ["PortChannel0001", "191300"], ctx, operator.ne)
+        assert 'Invalid port PortChannel0001' in result.output
     
     def test_config_tx_power(self, ctx):
         result = self.basic_check("tx_power", ["Ethernet0", "11.3"], ctx)
         assert "Setting target Tx output power" in result.output
         result = self.basic_check("tx_power", ["Ethernet0", "11.34"], ctx, op=operator.ne)
         assert "Error: tx power must be with single decimal place" in result.output
+        # Setting tx power on a port channel is not supported
+        result = self.basic_check("tx_power", ["PortChannel0001", "11.3"], ctx, operator.ne)
+        assert 'Invalid port PortChannel0001' in result.output
 
     def basic_check(self, command_name, para_list, ctx, op=operator.eq, expect_result=0):
         runner = CliRunner()
