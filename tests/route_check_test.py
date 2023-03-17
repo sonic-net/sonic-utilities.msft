f27dea0cfdefbdcfc03d19136e4ae47ea72fd51f
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2023-03-17 09:10:47 +0200

    [route_check] remove check-frr_patch mock (#2732)
    The test fails with python3.7 (works in 3.9) when stopping patch which hasn't been started. We can always mock check_output call and if FRR_ROUTES is not defined return empty dictionary by the mock.
    
    #### What I did
    
    Removed check_frr_patch mock to fix UT running on python3.7
    
    #### How I did it
    
    Removed the mock
    
    #### How to verify it
    
    Run unit test in stretch env

--- tests/route_check_test.py
+++ tests/route_check_test.py
@@ -277,17 +277,12 @@ class TestRouteCheck(object):
         with patch('sys.argv', ct_data[ARGS].split()), \
             patch('route_check.subprocess.check_output') as mock_check_output:
 
-            check_frr_patch = patch('route_check.check_frr_pending_routes', lambda: [])
+            routes = ct_data.get(FRR_ROUTES, {})
 
-            if FRR_ROUTES in ct_data:
-                routes = ct_data[FRR_ROUTES]
+            def side_effect(*args, **kwargs):
+                return json.dumps(routes)
 
-                def side_effect(*args, **kwargs):
-                    return json.dumps(routes)
-
-                mock_check_output.side_effect = side_effect
-            else:
-                check_frr_patch.start()
+            mock_check_output.side_effect = side_effect
 
             ret, res = route_check.main()
             expect_ret = ct_data[RET] if RET in ct_data else 0
@@ -298,8 +293,6 @@ class TestRouteCheck(object):
                 print("expect_res={}".format(json.dumps(expect_res, indent=4)))
             assert ret == expect_ret
             assert res == expect_res
-
-            check_frr_patch.stop()
 
     def test_timeout(self, mock_dbs, force_hang):
         # Test timeout
