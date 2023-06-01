b316fc27f5b56d7c04dc093ad55db600c4e6e2a3
longhuan-cisco <84595962+longhuan-cisco@users.noreply.github.com>
2023-05-31 22:01:34 -0700

    Add transceiver status CLI to show output from TRANSCEIVER_STATUS table (#2772)
    * Add transceiver status CLI to display TRANSCEIVER_STATUS table for CMIS/C-CMIS
    
    * Fix typo in command-ref
    
    * Add tx_disable
    
    * Update tx_disabled_channel's discription
    
    * Remove redundant code
    
    * addressed comments
    
    * Fix typo for tx power low warning
    
    * Separate CMIS and CCMIS status map
    
    * code cleanup
    
    * Unify display format and update display order
    
    * Fix after merge from master

--- show/interfaces/__init__.py
+++ show/interfaces/__init__.py
@@ -469,6 +469,29 @@ def pm(interfacename, namespace, verbose):
 
     clicommon.run_command(cmd, display_cmd=verbose)
 
+@transceiver.command('status') # 'status' is the actual sub-command name under 'transceiver' command
+@click.argument('interfacename', required=False)
+@click.option('--namespace', '-n', 'namespace', default=None, show_default=True,
+              type=click.Choice(multi_asic_util.multi_asic_ns_choices()), help='Namespace name or all')
+@click.option('--verbose', is_flag=True, help="Enable verbose output")
+def transceiver_status(interfacename, namespace, verbose):
+    """Show interface transceiver status information"""
+
+    ctx = click.get_current_context()
+
+    cmd = ['sfpshow', 'status']
+
+    if interfacename is not None:
+        interfacename = try_convert_interfacename_from_alias(
+            ctx, interfacename)
+
+        cmd += ['-p', str(interfacename)]
+
+    if namespace is not None:
+        cmd += ['-n', str(namespace)]
+
+    clicommon.run_command(cmd, display_cmd=verbose)
+
 @transceiver.command()
 @click.argument('interfacename', required=False)
 @click.option('--namespace', '-n', 'namespace', default=None, show_default=True,
