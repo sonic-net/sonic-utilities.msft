dc837194ad6ec82dccdd1819317390d1ac02da7a
Vadym Hlushko <62022266+vadymhlushko-mlnx@users.noreply.github.com>
2023-07-06 22:20:54 +0300

    [db_migrator] Set correct CURRENT_VERSION, extend UT (#2895)
    Signed-off-by: vadymhlushko-mlnx <vadymh@nvidia.com>
    
    What I did
    Set the correct value for the db_migrator class variable CURRENT_VERSION, because if the new DB version was introduced the CURRENT_VERSION variable should have the newest version value.
    
    How I did it
    Edit the db_migrator.py.
    
    How to verify it
    Extend existing UT.

--- tests/db_migrator_test.py
+++ tests/db_migrator_test.py
@@ -688,3 +688,4 @@ class TestFastUpgrade_to_4_0_3(object):
         dbmgtr.migrate()
         expected_db = self.mock_dedicated_config_db(db_after_migrate)
         assert not self.check_config_db(dbmgtr.configDB, expected_db.cfgdb)
+        assert dbmgtr.CURRENT_VERSION == expected_db.cfgdb.get_entry('VERSIONS', 'DATABASE')['VERSION']
