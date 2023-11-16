a8d236c8e3eee097a19e216a4c6a03ad56d6d734
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2023-11-16 23:26:45 +0200

    [fast-reboot-filter-routes.py] Remove click and improve error reporting (#3030)
    * [fast-reboot-filter-routes.py] Remove click and improve error reporting
    
    1. Removed click usage from a script since there is no active click
    context therefore we saw these error messages:
    
    ```
    ERR fast-reboot-filter-routes: Got an exception There is no active click context.: Traceback: Traceback (most recent call last):#012  File "/usr/local/bin/fast-reboot-filter-routes.py", line 18, in get_connected_routes#012    output, ret = clicommon.run_command(cmd, return_cmd=True)#012  File "/usr/local/lib/python3.9/dist-packages/utilities_common/cli.py", line 545, in run_command#012    proc = subprocess.Popen(command, shell=shell, text=True, stdout=subprocess.PIPE)#012  File "/usr/lib/python3.9/subprocess.py", line 951, in __init__#012    self._execute_child(args, executable, preexec_fn, close_fds,#012  File "/usr/lib/python3.9/subprocess.py", line 1823, in _execute_child#012    raise child_exception_type(errno_num, err_msg, err_filename)#012FileNotFoundError: [Errno 2] No such file or directory: 'sudo vtysh -c "show ip route connected json"'#012#012During handling of the above exception, another exception occurred:#012#012Traceback (most recent call last):#012  File "/usr/local/lib/python3.9/dist-packages/click/globals.py", line 23, in get_current_context#012    return getattr(_local, 'stack')[-1]#012AttributeError: '_thread._local' object has no attribute 'stack'#012#012During handling of the above exception, another exception occurred:#012#012Traceback (most recent call last):#012  File "/usr/local/bin/fast-reboot-filter-routes.py", line 79, in <module>#012    res = main()#012  File "/usr/local/bin/fast-reboot-filter-routes.py", line 70, in main#012    connected_routes = get_connected_routes()#012  File "/usr/local/bin/fast-reboot-filter-routes.py", line 27, in get_connected_routes#012    ctx = click.get_current_context()#012  File "/usr/local/lib/python3.9/dist-packages/click/globals.py", line 26, in get_current_context#012    raise RuntimeError('There is no active click context.')#012RuntimeError: There is no active click context.
    ```
    
    2. Improved error reporting so that when an error occurs when we run a
    command it will report it via terminal and syslog:
    
    stdout:
    ```
    Failed to execute sudo vtysh -c show ip route connected jsson: % Unknown command: show ip route connected jsson
    ```
    
    syslog:
    ```
    Oct 17 11:53:10.620788 arc-switch1025 ERR fast-reboot-filter-routes: Got an exception: Failed to execute sudo vtysh -c show ip route connected jsson: % Unknown command: show ip route connected jsson: Traceback: Traceback (most recent call last):#012  File "/home/admin/fast-reboot-filter-routes.py", line 73, in <module>#012    res = main()#012  File "/home/admin/fast-reboot-filter-routes.py", line 64, in main#012    connected_routes = get_connected_routes()#012  File "/home/admin/fast-reboot-filter-routes.py", line 18, in get_connected_routes#012    raise Exception("Failed to execute {}: {}".format(" ".join(cmd), output.rstrip('\n')))#012Exception: Failed to execute sudo vtysh -c show ip route connected jsson: % Unknown command: show ip route connected jsson
    ```
    
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>
    
    * add a test for command failure
    
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>
    
    * Increase coverage
    
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>
    
    ---------
    
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- tests/fast_reboot_filter_routes_test.py
+++ tests/fast_reboot_filter_routes_test.py
@@ -10,10 +10,17 @@ class TestFastRebootFilterRoutes(object):
 
     @patch('utilities_common.cli.run_command')
     def test_get_connected_routes(self, mock_run_command):
-        mock_run_command.return_value = (None, 0)
+        mock_run_command.return_value = ('{"1.1.0.0/16": {}}', 0)
         output = fast_reboot_filter_routes.get_connected_routes()
         mock_run_command.assert_called_with(['sudo', 'vtysh', '-c', "show ip route connected json"], return_cmd=True)
-        assert output == []
+        assert output == ['1.1.0.0/16']
+
+    @patch('utilities_common.cli.run_command')
+    def test_get_connected_routes_command_failed(self, mock_run_command):
+        mock_run_command.return_value = ('{"1.1.0.0/16": {}}', 1)
+        with pytest.raises(Exception):
+            fast_reboot_filter_routes.get_connected_routes()
+        mock_run_command.assert_called_with(['sudo', 'vtysh', '-c', "show ip route connected json"], return_cmd=True)
 
     def teardown(self):
         print("TEAR DOWN")
