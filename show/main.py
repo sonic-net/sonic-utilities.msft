1c094d302fa86924a3c72dea78fc599813f4eba0
jingwenxie <jingwenxie@microsoft.com>
2024-02-21 03:51:30 +0800

    [show] Update show run all to cover all asic config in multiasic (#3148)
    * [show] Update show run all to cover all asic config in masic
    
    * per comment

--- show/main.py
+++ show/main.py
@@ -142,6 +142,24 @@ def get_cmd_output(cmd):
     proc = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE)
     return proc.communicate()[0], proc.returncode
 
+def get_config_json_by_namespace(namespace):
+    cmd = ['sonic-cfggen', '-d', '--print-data']
+    if namespace is not None and namespace != multi_asic.DEFAULT_NAMESPACE:
+        cmd += ['-n', namespace]
+
+    stdout, rc = get_cmd_output(cmd)
+    if rc:
+        click.echo("Failed to get cmd output '{}':rc {}".format(cmd, rc))
+        raise click.Abort()
+
+    try:
+        config_json = json.loads(stdout)
+    except JSONDecodeError as e:
+        click.echo("Failed to load output '{}':{}".format(cmd, e))
+        raise click.Abort()
+
+    return config_json
+
 # Lazy global class instance for SONiC interface name to alias conversion
 iface_alias_converter = lazy_object_proxy.Proxy(lambda: clicommon.InterfaceAliasConverter())
 
@@ -1407,25 +1425,24 @@ def runningconfiguration():
 @click.option('--verbose', is_flag=True, help="Enable verbose output")
 def all(verbose):
     """Show full running configuration"""
-    cmd = ['sonic-cfggen', '-d', '--print-data']
-    stdout, rc = get_cmd_output(cmd)
-    if rc:
-        click.echo("Failed to get cmd output '{}':rc {}".format(cmd, rc))
-        raise click.Abort()
+    output = {}
+    bgpraw_cmd = "show running-config"
 
-    try:
-        output = json.loads(stdout)
-    except JSONDecodeError as e:
-        click.echo("Failed to load output '{}':{}".format(cmd, e))
-        raise click.Abort()
+    import utilities_common.bgp_util as bgp_util
+    # In multiaisc, the namespace is changed to 'localhost' by design
+    host_config = get_config_json_by_namespace(multi_asic.DEFAULT_NAMESPACE)
+    output['localhost'] = host_config
 
-    if not multi_asic.is_multi_asic():
-        bgpraw_cmd = [constants.RVTYSH_COMMAND, '-c', 'show running-config']
-        bgpraw, rc = get_cmd_output(bgpraw_cmd)
-        if rc:
-            bgpraw = ""
-        output['bgpraw'] = bgpraw
-    click.echo(json.dumps(output, indent=4))
+    if multi_asic.is_multi_asic():
+        ns_list = multi_asic.get_namespace_list()
+        for ns in ns_list:
+            ns_config = get_config_json_by_namespace(ns)
+            ns_config['bgpraw'] = bgp_util.run_bgp_show_command(bgpraw_cmd, ns)
+            output[ns] = ns_config
+        click.echo(json.dumps(output, indent=4))
+    else:
+        host_config['bgpraw'] = bgp_util.run_bgp_show_command(bgpraw_cmd)
+        click.echo(json.dumps(output['localhost'], indent=4))
 
 
 # 'acl' subcommand ("show runningconfiguration acl")
