73e12564e5ac7f7f94d67d4f284cce1d6e5d73f0
Marty Y. Lok <76118573+mlok-nokia@users.noreply.github.com>
2024-06-19 13:42:15 -0400

    [chassis][mutli-asic][lldp] "show lldp table/neighbors" displays error message in output on multi-asis linecard. (#3358)
    Modify the lldpshow script to fix the output of the "show lldp table" and "show lldp neighbors" on the multi-asic Linecard. Fixes sonic-net/sonic-buildimage#19209
    
    * [mutli-asic][lldp] "show lldp table" displays error message in output on multi-asic linecard
    
    * Added code coverage UT
    
    Signed-off-by: mlok <marty.lok@nokia.com>

--- utilities_common/general.py
+++ utilities_common/general.py
@@ -2,8 +2,11 @@ import importlib.machinery
 import importlib.util
 import sys
 
-from sonic_py_common.multi_asic import is_multi_asic
+from sonic_py_common import multi_asic
 from swsscommon import swsscommon
+FEATURE_TABLE = "FEATURE"
+FEATURE_HAS_PER_ASIC_SCOPE = 'has_per_asic_scope'
+FEATURE_HAS_GLOBAL_SCOPE = 'has_global_scope'
 
 def load_module_from_source(module_name, file_path):
     """
@@ -25,7 +28,7 @@ def load_db_config():
      - database_global.json for multi asic
      - database_config.json for single asic
     '''
-    if is_multi_asic():
+    if multi_asic.is_multi_asic():
         if not swsscommon.SonicDBConfig.isGlobalInit():
             swsscommon.SonicDBConfig.load_sonic_global_db_config()
     else:
@@ -39,6 +42,28 @@ def get_optional_value_for_key_in_config_tbl(config_db, port, key, table):
         return None
 
     value = info_dict.get(key, None)
-        
     return value
 
+
+def get_feature_state_data(config_db, feature):
+    '''
+    Get feature state from FEATURE table from CONFIG_DB.
+    return  global_scope, per_asic_scope
+    - if feature state is disabled, return "False" for both global_scope and per_asic_scope
+    - if is not a multi-asic, return feature state for global_scope ("True/False") and
+      "False" for asic_scope
+    '''
+    global_scope = "False"
+    asic_scope = "False"
+    info_dict = {}
+    info_dict = config_db.get_entry(FEATURE_TABLE, feature)
+    if info_dict is None:
+        return global_scope, asic_scope
+    if multi_asic.is_multi_asic():
+        if info_dict['state'].lower() == "enabled":
+            global_scope = info_dict[FEATURE_HAS_GLOBAL_SCOPE]
+            asic_scope = info_dict[FEATURE_HAS_PER_ASIC_SCOPE]
+    else:
+        if info_dict['state'].lower() == "enabled":
+            global_scope = "True"
+    return global_scope, asic_scope
