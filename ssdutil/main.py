d64a90a02ffe07ef0bec060a4df03a7b2f1ada5a
Ashwin Srinivasan <93744978+assrinivasan@users.noreply.github.com>
2024-10-27 19:19:38 -0700

    Adds logic to get default disk and check disk type (#3399)
    * Added function to check if disk is SATA
    
    * Fixed eval bug unconvered by UT
    
    * Changed logger to syslogger; fixed UT failures
    
    * Added checks for partitions and filtered_disks; fixed static analysis issues
    
    * Fix static analysis errors E303, E711
    
    * Changed information delivery semantics per review comment
    
    * Reverted syslogger to logger to maintain backward compatibility
    
    The syslogger-to-logger change is unrelated to the sonic-utilities
    change and breaks backwards compatibility. It should be a separate
    commit once SysLogger is in all the older versions.
    
    * Changed Disk 'type' --> 'Type' for uniformity
    
    * Made the fields look uniform
    
    * Disk Type Support for eMMC devices
    
    * Removed old sonic_ssd import

--- ssdutil/main.py
+++ ssdutil/main.py
@@ -6,19 +6,59 @@
 #
 
 try:
-    import argparse
     import os
     import sys
+    import argparse
+    import psutil
+    from blkinfo import BlkDiskInfo
 
     from sonic_py_common import device_info, logger
 except ImportError as e:
     raise ImportError("%s - required module not found" % str(e))
 
-DEFAULT_DEVICE="/dev/sda"
+DEFAULT_DEVICE = "/dev/sda"
 SYSLOG_IDENTIFIER = "ssdutil"
+DISK_TYPE_SSD = "sata"
 
 # Global logger instance
 log = logger.Logger(SYSLOG_IDENTIFIER)
+
+
+def get_default_disk():
+    """Check default disk"""
+    default_device = DEFAULT_DEVICE
+    host_mnt = '/host'
+    host_partition = None
+    partitions = psutil.disk_partitions()
+
+    if partitions is None:
+        return (default_device, None)
+
+    for parts in partitions:
+        if parts.mountpoint == host_mnt:
+            host_partition = parts
+            break
+
+    disk_major = os.major(os.stat(host_partition.device).st_rdev)
+    filters = {
+       'maj:min': '{}:0'.format(disk_major)
+    }
+
+    myblkd = BlkDiskInfo()
+    my_filtered_disks = myblkd.get_disks(filters)
+
+    if my_filtered_disks is None:
+        return (default_device, None)
+
+    json_output = my_filtered_disks[0]
+    blkdev = json_output['name']
+    disk_type = json_output['tran']
+    default_device = os.path.join("/dev/", blkdev)
+
+    # Disk Type Support for eMMC devices
+    disk_type = 'eMMC' if len(disk_type) == 0 and 'mmcblk' in host_partition.device else disk_type  # noqa: E501
+
+    return default_device, disk_type
 
 
 def import_ssd_api(diskdev):
@@ -37,14 +77,15 @@ def import_ssd_api(diskdev):
         sys.path.append(os.path.abspath(platform_plugins_path))
         from ssd_util import SsdUtil
     except ImportError as e:
-        log.log_warning("Platform specific SsdUtil module not found. Falling down to the generic implementation")
+        log.log_warning("Platform specific SsdUtil module not found. Falling down to the generic implementation")  # noqa: E501
         try:
             from sonic_platform_base.sonic_storage.ssd import SsdUtil
         except ImportError as e:
-            log.log_error("Failed to import default SsdUtil. Error: {}".format(str(e)), True)
+            log.log_error("Failed to import default SsdUtil. Error: {}".format(str(e)), True)  # noqa: E501
             raise e
 
     return SsdUtil(diskdev)
+
 
 def is_number(s):
     try:
@@ -53,6 +94,7 @@ def is_number(s):
     except ValueError:
         return False
 
+
 # ==================== Entry point ====================
 def ssdutil():
     if os.geteuid() != 0:
@@ -60,21 +102,24 @@ def ssdutil():
         sys.exit(1)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--device", help="Device name to show health info", default=DEFAULT_DEVICE)
-    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Show verbose output (some additional parameters)")
-    parser.add_argument("-e", "--vendor", action="store_true", default=False, help="Show vendor output (extended output if provided by platform vendor)")
+    (default_device, disk_type) = get_default_disk()
+    parser.add_argument("-d", "--device", help="Device name to show health info", default=default_device)  # noqa: E501
+    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Show verbose output (some additional parameters)")  # noqa: E501
+    parser.add_argument("-e", "--vendor", action="store_true", default=False, help="Show vendor output (extended output if provided by platform vendor)")  # noqa: E501
     args = parser.parse_args()
 
+    print("Disk Type    : {0}".format(disk_type.upper()))
     ssd = import_ssd_api(args.device)
 
     print("Device Model : {}".format(ssd.get_model()))
     if args.verbose:
         print("Firmware     : {}".format(ssd.get_firmware()))
         print("Serial       : {}".format(ssd.get_serial()))
-    print("Health       : {}{}".format(ssd.get_health(),      "%" if is_number(ssd.get_health()) else ""))
-    print("Temperature  : {}{}".format(ssd.get_temperature(), "C" if is_number(ssd.get_temperature()) else ""))
+    print("Health       : {}{}".format(ssd.get_health(),      "%" if is_number(ssd.get_health()) else ""))  # noqa: E501
+    print("Temperature  : {}{}".format(ssd.get_temperature(), "C" if is_number(ssd.get_temperature()) else ""))  # noqa: E501
     if args.vendor:
         print(ssd.get_vendor_output())
+
 
 if __name__ == '__main__':
     ssdutil()
--- tests/ssdutil_test.py
+++ tests/ssdutil_test.py
@@ -1,8 +1,21 @@
+import os
 import sys
 import argparse
 from unittest.mock import patch, MagicMock
 import sonic_platform_base  # noqa: F401
 
+tests_path = os.path.dirname(os.path.abspath(__file__))
+
+# Add mocked_libs path so that the file under test
+# can load mocked modules from there
+mocked_libs_path = os.path.join(tests_path, "mocked_libs")  # noqa: E402,F401
+sys.path.insert(0, mocked_libs_path)
+
+from .mocked_libs import psutil  # noqa: E402,F401
+from .mocked_libs.blkinfo import BlkDiskInfo  # noqa: E402,F401
+
+sys.modules['os.stat'] = MagicMock()
+sys.modules['os.major'] = MagicMock(return_value=8)
 sys.modules['sonic_platform'] = MagicMock()
 sys.modules['sonic_platform_base.sonic_ssd.ssd_generic'] = MagicMock()
 
@@ -32,8 +45,33 @@ class Ssd():
 
 class TestSsdutil:
 
+    @patch('os.geteuid', MagicMock(return_value=0))
+    @patch('os.stat', MagicMock(st_rdev=2049))
+    @patch('os.major', MagicMock(return_value=8))
+    def test_get_default_disk(self):
+        (default_device, disk_type) = ssdutil.get_default_disk()
+
+        assert default_device == "/dev/sdx"
+        assert disk_type == 'usb'
+
+    @patch('os.geteuid', MagicMock(return_value=0))
+    @patch('os.stat', MagicMock(st_rdev=2049))
+    @patch('os.major', MagicMock(return_value=8))
+    @patch('psutil.disk_partitions', MagicMock(return_value=None))
+    def test_get_default_disk_none_partitions(self):
+        (default_device, disk_type) = ssdutil.get_default_disk()
+
+        assert default_device == "/dev/sda"
+        assert disk_type is None
+
+    def test_is_number_valueerror(self):
+        outcome = ssdutil.is_number("nope")
+        assert outcome is False
+
     @patch('sonic_py_common.device_info.get_paths_to_platform_and_hwsku_dirs', MagicMock(return_value=("test_path", "")))  # noqa: E501
     @patch('os.geteuid', MagicMock(return_value=0))
+    @patch('os.stat', MagicMock(st_rdev=2049))
+    @patch('os.major', MagicMock(return_value=8))
     def test_sonic_storage_path(self):
 
         with patch('argparse.ArgumentParser.parse_args', MagicMock()) as mock_args:  # noqa: E501
